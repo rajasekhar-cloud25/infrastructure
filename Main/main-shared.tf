468627451a3bbd1be61e7c49fa3233a2fab0b967
rajasekhar.reddy <rajasekhar.reddy@ui.city>
2026-03-31 23:43:11 +0530

    updated the public subnet

--- Main/main-shared.tf
+++ Main/main-shared.tf
@@ -55,7 +55,7 @@ module "kubernetes_ingress" {
   depends_on              = [module.eks, module.namespaces]
   domain_name             = var.domain_name
   dns_names               = var.dns_names
-  public_subnet_ids       = module.vpc.private_subnet_ids
+  public_subnet_ids       = module.vpc.public_subnet_ids
 }
 
 module "namespaces" {
