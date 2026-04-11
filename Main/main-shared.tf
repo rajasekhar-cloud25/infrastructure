bc05cb9855dd05148205a2729df245d26478b822
rajasekhar.reddy <rajasekhar.reddy@ui.city>
2026-04-11 15:46:29 +0530

    fix cermanager

--- Main/main-shared.tf
+++ Main/main-shared.tf
@@ -76,4 +76,5 @@ module "argocd_deployment" {
 module "cert_manager" {
   source = "../cert-manager"
   cert_manager_role_arn = module.iam.cert_manager_role_arn
+  depends_on   = [module.eks, module.namespaces]
 }
